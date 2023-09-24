aa521f0af688186ae2df6f7120b56a293b040f9d
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2023-09-24 15:18:56 +0530

    added django files and integrated html css javascript files.

---
+++ Auth/registration/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    pass
