ecbbbeb385966b4a6d2a6724454e0c73eea8df89
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2023-09-24 20:07:40 +0530

    view.py file updated with login authentication features.

--- Auth/registration/views.py
+++ Auth/registration/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
@@ -17,6 +17,9 @@ def signup(request):
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
+
+        if pass1!=pass2:
+            return HttpResponse("Your password are not same.")
 
         myuser = User.objects.create_user(username, email, pass1)
         myuser.name = name
@@ -35,12 +38,12 @@ def signin(request):
         username = request.POST['username']
         pass1 = request.POST['pass1']
 
-        user = authenticate(username=username, password=pass1)
+        user = authenticate(request, username=username, password=pass1)
 
-        if user is None:
+        if user is not None:
             login(request, user)
-            name = user.name
-            return render(request, "authentication/index.html", {'name':name})
+            name = user.first_name
+            return render(request, "index.html", {'name':name})
 
         else:
             messages.error(request, "Incorrect Username & Password.")
@@ -49,5 +52,7 @@ def signin(request):
 
     return render(request, 'signin.html')
 
-def logout(request):
-    pass
+def signout(request):
+    logout(request)
+    messages.success(request, "logged out successfully!")
+    return redirect('home')
